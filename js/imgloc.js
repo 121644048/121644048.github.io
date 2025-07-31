5f05855719d98b3852584fabd6ad18be38e70b42
caobaoning <121644048@qq.com>
2025-07-31 10:47:22 +0800

    Site updated: 2025-07-31 10:47:19

--- js/imgloc.js
+++
@@ -1,8 +0,0 @@
-// 引入 ImgLoc 上传插件
-(function() {
-  var script = document.createElement('script');
-  script.async = true;
-  script.src = 'https://imgloc.com/sdk/pup.js';
-  script.dataset.url = 'https://imgloc.com/upload';
-  document.head.appendChild(script);
-})();
