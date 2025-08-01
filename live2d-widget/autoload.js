dfda78fcca8061f1465900619ae424979ef1fb3e
caobaoning <121644048@qq.com>
2025-08-01 17:50:32 +0800

    Site updated: 2025-08-01 17:50:10

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -35,7 +35,7 @@ if (screen.width >= 768) {
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
 			//cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
-			cdnPath: "https://live2d-api.vercel.app/"
+			cdnPath: "https://npm.elemecdn.com/akilar-live2dapi@latest/"
 		});
 	});
 }
